781f8f3264c05c556ae773f017c7db0141ee8486
Filip Jahn <filip.jahn.cz@gmail.com>
2026-04-19 10:44:43 +0200

    2 quads working, finding fire, but too close to each other

--- demos/MAPPO_easiestScenario/demo_scout.py
+++ demos/MAPPO_easiestScenario/demo_scout.py
@@ -23,10 +23,10 @@ def run_demo():
     MAX_STEPS = 500
     
     # Který model chceme načíst? (Změň číslo podle toho, který měl u tebe nejlepší Reward)
-    MODEL_PATH = "/homes/eva/xj/xjahnf00/tmp/DP/saved_models/scout_solo/scout_b1000.pt"
+    MODEL_PATH = "/homes/eva/xj/xjahnf00/tmp/DP/saved_models/scout_solo/scout_b0060.pt"
     
     # 2. Inicializace prostředí
-    env = DroneFireEnv(num_quads=N_QUADS, num_fixed=N_FIXED, grid_size_m=800.0, max_steps=MAX_STEPS)
+    env = DroneFireEnv(num_quads=N_QUADS, num_fixed=N_FIXED, grid_size_m=900.0, max_steps=MAX_STEPS)
     
     # Zjištění dimenzí (stejné jako v train.py)
     obs_q = env.observation_space("quad_0")
